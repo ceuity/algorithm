812f6ccfec398b038cfc55a175da5e379db4592d
hyulee <hyulee@student.42seoul.kr>
2021-01-15 02:29:31 +0900

    Update 5

--- leetcode/5.py
+++ leetcode/5.py
@@ -7,7 +7,16 @@ Created on Fri Jan 15 02:00:14 2021
 
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        length = len(str)
+        length = len(s)
+        const = length + 1
         while length > 0:
-            for i in range(length):
-                if str[i:length-1] == 
+            for i in range(const - length):
+                temp = s[i:i+length]
+                if temp == temp[::-1]:
+                    return temp;
+            length -= 1
+                    
+s = "babad"
+
+ret = Solution()
+print(ret.longestPalindrome(s))
