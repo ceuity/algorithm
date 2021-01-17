1c9469889d7e480371b7dac68e096bb4d0ba1b85
hyulee <hyulee@student.42seoul.kr>
2021-01-18 03:26:44 +0900

    Update: 210118

--- leetcode/1.py
+++ leetcode/1.py
@@ -10,3 +10,6 @@ class Solution:
             for j in range(i + 1, len(nums)):
                 if nums[i] + nums[j] == target:
                     return [i, j]
+                
+ret = Solution()
+print(ret.twoSum(s))
