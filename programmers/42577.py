be2ac06bca57d574d0de2e427d08bf8baa0fcc91
ceuity <everland7942@gmail.com>
2021-03-19 22:40:53 +0900

    Update: add testcase

--- programmers/42577.py
+++ programmers/42577.py
@@ -4,7 +4,7 @@ Created on Thu Dec 10 16:15:53 2020
 
 @author: EVer
 """
-
+""" 테스트 케이스가 바뀌어 아래 코드는 더이상 통과할 수 없었다.
 def solution(phone_book):
     for i in range(len(phone_book)):
         for j in range(i + 1, len(phone_book)):
@@ -13,6 +13,15 @@ def solution(phone_book):
                 return False
     answer = True
     return answer
+"""
 
-phone_book = ['123', '456', '789']
-print(solution(phone_book))
+def solution(phone_book):
+    phone_book.sort()
+    for p1, p2 in zip(phone_book, phone_book[1:]):
+        if p2.startswith(p1):
+            return False
+    return True
+
+if __name__ == '__main__':
+    phone_book = ["119", "97674223", "1195524421"]
+    print(solution(phone_book))
